b4798adf8b39e4344ef320b751e3aabbf18dad96
isatimur <isatimur.it@gmail.com>
2026-02-28 22:56:01 +0000

    build: add gRPC + protobuf dependencies and agent.proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25" apply false
     kotlin("plugin.serialization") version "1.9.25" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
 }
 
 subprojects {
